7e5dc49afed9d362ffe32e9a72b7a62236a016a6
premkumar-juhomi <premkumar29779@gmail.com>
2018-05-03 19:20:07 +0530

    difference of double quotes with numericals

--- ex1to.rb
+++ ex1to.rb
@@ -12,7 +12,7 @@ puts "Hens #{25 + 30 / 6}"#{hash tag followed by braces gives VALUE}
 puts "Roosters #{100 - 25 * 3 % 4}"
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 puts "Is it true that 3 + 2 < 5 - 7?"
-puts 3 + 2 < 5 - 7
+puts 3 + 2 < 5 - 7 # "double quotes" alone makes as string
 puts "What is 5 - 7? #{5 - 7}"
 puts "Is it greater? #{5 > -2}"
 puts "Is it less or equal? #{5 <= -2}"
