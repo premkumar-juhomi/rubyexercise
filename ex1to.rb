b28eae67c89fca68d63e9c4456b7a6540c6faa1b
premkumar-juhomi <premkumar29779@gmail.com>
2018-05-03 19:14:34 +0530

    hash tag braces with values

--- ex1to.rb
+++ ex1to.rb
@@ -8,7 +8,7 @@ puts "hello first line
 #lines.split("\n").reverse_each do |line|
 #    puts line
 puts "I will now count my chickens:"
-puts "Hens #{25 + 30 / 6}"#{hash tag followed by braces gives results}
+puts "Hens #{25 + 30 / 6}"#{hash tag followed by braces gives VALUE}
 puts "Roosters #{100 - 25 * 3 % 4}"
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 puts "Is it true that 3 + 2 < 5 - 7?"
